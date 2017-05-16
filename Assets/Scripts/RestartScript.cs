2c5ccad28557144c0f71f434e6c72ed7c0090258
josh schank <jps364@drexel.edu>
2017-05-16 13:03:28 -0400

    Updated Input Manager for controllers and Restart script

--- Assets/Scripts/RestartScript.cs
+++ Assets/Scripts/RestartScript.cs
@@ -11,7 +11,7 @@ public class RestartScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKeyDown(KeyCode.Backspace))
+		if(Input.GetButton("Restart")||Input.GetKeyDown(KeyCode.Backspace))
 		{
 			//Debug.Log ("ReloadScene");
 			Application.LoadLevel (Application.loadedLevel);
